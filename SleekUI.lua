0e90c36bf7ce5357b49dc11106795f5e63c8e652
Kyle Scott <kyle.j.scott@hotmail.com>
2020-04-02 19:44:32 -0400

    write some debugging tools

--- SleekUI.lua
+++ SleekUI.lua
@@ -1,1 +1,18 @@
+-- Debugging Tools
+SLASH_RELOAD = '/rl' -- for quick relading of the UI
+
+SLASH_FRAMESTK = '/fs' -- for quick access to the frame stack
+
+SlashCmdList.FRAMESTK = function()
+	LoadAddOn('Blizzard_DebugTools');
+	FrameStackTooltip_Toggle();
+end
+
+for i = 1, NUM_CHAT_WINDOWS do
+	_G['ChatFrame"..i.."EditBox']:SetAltArrowKeyMode(false);
+end
+
+---------------------
+-- Actual Addon 
+
 print('Hello' .. UnitName('player') .. '' .. UnitLevel('player'));
